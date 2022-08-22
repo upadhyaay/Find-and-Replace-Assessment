a45e3d960f11dfac581856bd2fdcedc5fa907a8d
Shashank UPADHYAY <shashank.upadhyay@chanel.com>
2022-08-23 00:51:11 +0530

    Add project files.

---
+++ WebApplication2/Controllers/FnRController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class FnRController : ApiController
+    {
+        public string Deltest(string inputstring)
+        {
+            foreach (string eachword in inputstring.Split(new char[] { ' ', ',', '.', '-', ':', '\n', '\t' }))
+            {
+                System.Text.StringBuilder builder = new System.Text.StringBuilder(eachword);
+                if (eachword == "Google" || eachword == "Oracle" || eachword == "Deloitte" || eachword == "Amazon" || eachword == "Microsoft")
+                {
+                    string temp = builder.Append("Ⓒ").ToString();
+                    inputstring = inputstring.Replace(eachword, temp);
+                }
+            }
+            return inputstring;
+        }
+    }
+}
